1e83f1ffa20bc50f21a96250205f9d4e4133ec90
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-09-18 14:48:29 +0200

    Router config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'crud', pathMatch: 'full'},
+  {path: 'crud', loadChildren: ()=> import('./crud/crud.module').then(m=>m.CrudModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
